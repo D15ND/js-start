f8f3b17c0c5631d1309ebf17f837570480d6e8ee
ILYA <106765406+D15ND@users.noreply.github.com>
2022-09-19 22:00:37 +0300

    redact js function

--- z.js
+++ z.js
@@ -1,1 +1,8 @@
-// clear page
+const outerWear = "T-Shirt";
+function myOutfit() {
+const outerWear=('sweater');
+    console.log('local output:', outerWear);
+    return outerWear;
+}
+myOutfit();
+console.log('global output:', outerWear);
